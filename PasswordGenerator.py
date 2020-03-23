952a3a48330274d033a19e53dfad98c599ae0e89
Pilljam <62140466+Pilljam@users.noreply.github.com>
2020-03-23 20:10:31 +0100

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,84 @@
+import random
+import string
+
+
+def password_length(valid):
+    wanted_password = int(input("What length would you like your password to be? It needs to be at least 6 characters: "))
+    while wanted_password != valid:
+        if wanted_password > 5:
+            valid = wanted_password
+            users_look(valid)
+        elif wanted_password < 6:
+            print("Invalid length!")
+            wanted_password = int(input("New length: "))
+
+
+def password_generator(string_length=6):
+    digitsAndLetters = string.ascii_letters + string.digits
+    return ''.join(random.choice(digitsAndLetters) for i in range(string_length))
+
+
+def users_look(valid):
+    print("Let´s generate some passwords:")
+    print("First random password is " + password_generator(valid))
+    print("Second random password is " + password_generator(valid))
+    print("Third random password is " + password_generator(valid))
+    user_pleased()
+
+
+def user_pleased():
+    pleased = input("Are you pleased with these passwords? Yes or no: ").lower()
+    if pleased == 'no':
+        print("Let´s generate som new passwords!")
+        password_length(valid=150)
+    else:
+        exit()
+
+
+password_length(valid=150)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
